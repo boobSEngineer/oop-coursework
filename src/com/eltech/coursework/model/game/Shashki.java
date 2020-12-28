7b36629f952ef496de5586162d1acec31936a9a9
Helen <lezu2000@mail.ru>
2020-12-28 23:20:51 +0300

    eating

--- src/com/eltech/coursework/model/game/Shashki.java
+++ src/com/eltech/coursework/model/game/Shashki.java
@@ -4,6 +4,7 @@ import com.eltech.coursework.model.Figure;
 import com.eltech.coursework.model.GameField;
 import javafx.util.Pair;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,26 +37,33 @@ public class Shashki implements GameRules {
 
     @Override
     public List<Pair<Integer, Integer>> getAvailableMoves(Figure figure) {
-        List<Pair<Integer, Integer>> moves = new ArrayList<>();
+        List<Pair<Integer, Integer>> normalMoves = new ArrayList<>();
+        List<Pair<Integer, Integer>> eatingMoves = new ArrayList<>();
 
         GameField field = figure.getField();
         int x = figure.getX();
         int y = figure.getY();
         for (int xi = -1; xi < 2; xi += 2) {
             for (int yi = -1; yi < 2; yi += 2) {
-                if (field.getFigure(x + xi, y + yi) == null) {
-                    moves.add(new Pair<>(x + xi, y + yi));
-                } else if (field.getFigure(x + xi * 2, y + yi * 2) == null) {
-                    moves.add(new Pair<>(x + xi * 2, y + yi * 2));
+                Figure eatFigure = field.getFigure(x + xi, y + yi);
+                if (eatFigure == null) {
+                    normalMoves.add(new Pair<>(x + xi, y + yi));
+                } else if (eatFigure.getTeam() != figure.getTeam() && field.getFigure(x + xi * 2, y + yi * 2) == null) {
+                    eatingMoves.add(new Pair<>(x + xi * 2, y + yi * 2));
                 }
             }
         }
 
-        return moves;
+        return eatingMoves.size() > 0 ? eatingMoves : normalMoves;
     }
 
     @Override
     public void doMove(Figure figure, int x, int y) {
+        if (Math.abs(x - figure.getX()) > 1 || Math.abs(y - figure.getY()) > 1) {
+            int eatX = (figure.getX() + x) / 2;
+            int eatY = (figure.getY() + y) / 2;
+            figure.getField().setFigure(eatX, eatY, null);
+        }
         figure.move(x, y);
         currentTeam = (currentTeam == Figure.Team.WHITE ? Figure.Team.BLACK : Figure.Team.WHITE);
     }
